c53f45e2ba72acdaabbda240043bb3624500cc34
Oleg Kuzyk <qzix13@gmail.com>
2020-10-07 10:04:52 +0300

    format the signals

--- NepixSignalsUnityProject/Assets/Plugins/NepixSignals/Runtime/TheSignals.cs
+++ NepixSignalsUnityProject/Assets/Plugins/NepixSignals/Runtime/TheSignals.cs
@@ -7,33 +7,15 @@ namespace NepixSignals
     /// <summary>
     /// Default signals. If you don't want to create simple signals or use a hub.
     /// </summary>
-    public class TheSignal : ASignal
-    {
-        
-    }
+    public class TheSignal : ASignal { }
 
-    public class TheSignalInt : ASignal<int>
-    {
-        
-    }
-    public class TheSignalDouble : ASignal<double>
-    {
-        
-    }
+    public class TheSignalInt : ASignal<int> { }
+    public class TheSignalDouble : ASignal<double> { }
 
-    public class TheSignalFloat : ASignal<float>
-    {
-        
-    }
+    public class TheSignalFloat : ASignal<float> { }
 
-    public class TheSignalBool : ASignal<bool>
-    {
-        
-    }
+    public class TheSignalBool : ASignal<bool> { }
 
-    public class TheSignalColor : ASignal<Color>
-    {
-        
-    }
+    public class TheSignalColor : ASignal<Color> { }
 
 }
