e5a9580a7e51a00c67354733c658dc44c74549b9
Oleg Kuzyk <qzix13@gmail.com>
2020-10-07 10:14:36 +0300

    Packager added

---
+++ NepixSignalsUnityProject/Assets/Editor/Packager/Packager.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+#pragma warning disable 0649
+namespace DefaultNamespace
+{
+    public static class UnityPackageUtil
+    {
+        [MenuItem("Tools/Package Signals")]
+        public static void CreateUnityPackage()
+        {
+            var outPath = Path.GetFullPath(Path.Combine(Application.dataPath, "../NepixSignals.unitypackage"));
+            AssetDatabase.ExportPackage("Assets/Plugins/NepixSignals", outPath, ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
+            EditorUtility.RevealInFinder(outPath);
+            Debug.Log("Exported to " + outPath);
+        }
+    }
+}
