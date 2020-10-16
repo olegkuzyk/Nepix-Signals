dec20985318198199925bd6440b0e1e7a9ff4ccc
Oleg Kuzyk <qzix13@gmail.com>
2020-10-17 01:48:06 +0300

    format file

--- NepixSignalsUnityProject/Assets/Plugins/NepixSignals/Runtime/Api/ISignalCallback.cs
+++ NepixSignalsUnityProject/Assets/Plugins/NepixSignals/Runtime/Api/ISignalCallback.cs
@@ -9,60 +9,63 @@ namespace NepixSignals.Api
     /// </summary>
     public interface ISignalCallback
     {
-            int priority { get; }
-            
-            /// <summary>
-            /// How many times it should be called.
-            /// Negative value means infinite calls amount.
-            /// </summary>
-            int countdown { get; }
+        /// <summary>
+        /// Priority of callback.
+        /// </summary>
+        int priority { get; }
 
-            /// <summary>
-            /// Check if callback added to a signal.
-            /// </summary>
-            /// <returns></returns>
-            bool IsAddedToSignal();
+        /// <summary>
+        /// How many times left to call before remove.
+        /// Negative value means infinite calls amount.
+        /// </summary>
+        int countdown { get; }
 
-            /// <summary>
-            /// On again the callback to signal, in case it was off.
-            /// Warning: exception could be fired
-            /// if signal already has this callback.
-            /// </summary>
-            void On();
+        /// <summary>
+        /// Check if callback added to a signal.
+        /// </summary>
+        /// <returns></returns>
+        bool IsAddedToSignal();
 
-            /// <summary>
-            /// Once again the callback to signal, in case it was off.
-            /// Warning: exception could be fired
-            /// if signal already has this callback.
-            /// </summary>
-            void Once();
+        /// <summary>
+        /// On again the callback to signal, in case it was off.
+        /// Warning: exception could be fired
+        /// if signal already has this callback.
+        /// </summary>
+        void On();
 
-            /// <summary>
-            /// Off from signal.
-            /// </summary>
-            void Off();
+        /// <summary>
+        /// Once again the callback to signal, in case it was off.
+        /// Warning: exception could be fired
+        /// if signal already has this callback.
+        /// </summary>
+        void Once();
 
-            /// <summary>
-            /// The handler will be called only when predicate fulfilled.
-            /// </summary>
-            /// <param name="predicate">When predicate</param>
-            /// <returns>Callback</returns>
-            ISignalCallback When(Func<bool> predicate);
+        /// <summary>
+        /// Off from signal.
+        /// </summary>
+        void Off();
 
-            /// <summary>
-            /// Set priority of callback.
-            /// </summary>
-            /// <param name="value"></param>
-            /// <returns></returns>
-            ISignalCallback Priority(int value);
+        /// <summary>
+        /// The handler will be called only when predicate fulfilled.
+        /// </summary>
+        /// <param name="predicate">When predicate</param>
+        /// <returns>Callback</returns>
+        ISignalCallback When(Func<bool> predicate);
 
-            /// <summary>
-            /// Set countdown for callback.
-            /// How many times it should be called.
-            /// Negative value means infinite calls amount.
-            /// </summary>
-            /// <param name="value"></param>
-            /// <returns></returns>
-            ISignalCallback Countdown(int value);
+        /// <summary>
+        /// Set priority of callback.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        ISignalCallback Priority(int value);
+
+        /// <summary>
+        /// Set countdown for callback.
+        /// How many times it should be called.
+        /// Negative value means infinite calls amount.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        ISignalCallback Countdown(int value);
     }
 }
